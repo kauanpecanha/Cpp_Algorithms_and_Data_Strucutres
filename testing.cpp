312fbadb1df9c274f4b2776503a4c3ff57797df6
KauanPecanha <kauanpecanha1@gmail.com>
2022-12-04 10:10:17 -0300

    First Commit

---
+++ testing.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+// class employee{
+//     public:
+//         char matricula;
+//         char nome;
+//         char dataNasc;
+
+//         employee(){ // construtor padrão
+
+//             matricula = '';
+//             nome = '';
+//             dataNasc = '';
+
+//         }
+
+//         employee(char m, char n, char d){ // construtor parametrizado
+
+//             matricula = m;
+//             nome  = n;
+//             dataNasc = d;
+
+//         }
+// }
+
+// int main(){
+    
+//     employee f1 = new employee('202110048911', 'kauan', '25/05/2004');
+    
+//     return 0;
+// }
+
+int main(){
+
+    cout<<"Kauan Peçanha"<<endl;
+
+    return 0;
+}
